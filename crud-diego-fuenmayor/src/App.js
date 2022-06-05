6be0e4fa65a6883a6db8d1659fb3cada99c2fdf4
Diego Fuenmayor <dfuenmayor76@gmail.com>
2022-06-05 19:36:43 -0400

    finalizada actividad 1 react

--- crud-diego-fuenmayor/src/App.js
+++ crud-diego-fuenmayor/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter } from 'reactstrap';
+import { Modal, ModalBody, ModalHeader, ModalFooter } from 'reactstrap';
 
 function App() {
 
@@ -16,202 +16,124 @@ function App() {
     { "value": "Indefinido" }
   ]
 
-  const [nombre, setNombre] = useState('');
-  const [apellido, setApellido] = useState('');
-  const [cedula, setCedula] = useState('');
-  const [birthday, setBirthday] = useState('');
-  const [seccion, setSeccion] = useState('');
-  const [genero, setGenero] = useState('');
-  const [students, setStudents] = useState([])
-
+  const [student, setStudent] = useState([]);
+  const [modalVer, setModalVer] = useState(false);
+  const [modalInsertar, setModalInsertar] = useState(false);
   const [modalEditar, setModalEditar] = useState(false);
   const [modalEliminar, setModalEliminar] = useState(false);
 
-  // Metodos
 
-  const addStudents = () => {
-    if (nombre === "") {
-      alert(`Ingrese el nombre`)
-    }
-    else if (apellido === "") {
-      alert(`Ingrese el apellido`)
-    }
-    else if (cedula === "") {
-      alert(`Ingrese el apellido`)
-    }
-    else if (seccion === "") {
-      alert(`Ingrese el apellido`)
-    }
-    else if (genero === "") {
-      alert(`Ingrese el apellido`)
-    }
-    else if (isNaN(cedula)) {
-      alert(`La cedula solamente puede ser numerica`)
-    }
-    else {
-      const jsn = {
-        "nombre": nombre,
-        "apellido": apellido,
-        "cedula": cedula,
-        "birthday": birthday,
-        "seccion": seccion,
-        "genero": genero
-      }
-
-
-      const arrStudents = [...students];
-      arrStudents.push(jsn);
-      setStudents(arrStudents);
-
-      setNombre('');
-      setApellido('');
-      setCedula('');
-      setBirthday('');
-      setSeccion('');
-      setGenero('');
-    }
-  }
-
-  const [studentSelected, setStudentSelected] = useState({
+  const [estudianteSeleccionado, setEstudianteSeleccionado] = useState({
     cedula: '',
     nombre: '',
     apellido: '',
     birthday: '',
-    seccion: '',
     genero: '',
+    seccion: ''
   });
 
   const seleccionarEstudiante = (elemento, caso) => {
-    setStudentSelected(elemento);
-    (caso === 'Editar') ? setModalEditar(true) : setModalEliminar(true)
+    setEstudianteSeleccionado(elemento);
+    switch (caso) {
+      case 'Ver':
+        setModalVer(true);
+        break;
+      case 'Editar':
+        setModalEditar(true)
+        break;
+      case 'Eliminar':
+        setModalEliminar(true)
+        break;
+
+      default:
+        return;
+    }
   }
 
-  const handleChange=e=>{
-    const {cedula, nombre, apellido, value}=e.target;
-    setStudentSelected((prevState)=>({
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setEstudianteSeleccionado((prevState) => ({
       ...prevState,
-      [cedula]: value,
-      [nombre]: value,
-      [apellido]: value
+      [name]: value
     }));
   }
 
-  const editar=()=>{
-    var dataNueva=students;
-    dataNueva.map(estudiante=>{
-      if(estudiante.cedula===studentSelected.cedula){
-        estudiante.apellido=studentSelected.apellido;
-        estudiante.nombre=studentSelected.nombre;
+  const editar = () => {
+    var dataNueva = student;
+    dataNueva.map(estudiante => {
+      if (estudiante.cedula === estudianteSeleccionado.cedula) {
+        estudiante.nombre = estudianteSeleccionado.nombre;
+        estudiante.apellido = estudianteSeleccionado.apellido;
+        estudiante.birthday = estudianteSeleccionado.birthday;
+        estudiante.genero = estudianteSeleccionado.genero;
+        estudiante.seccion = estudianteSeleccionado.seccion;
       }
     });
-    setStudents(dataNueva);
+    setStudent(dataNueva);
     setModalEditar(false);
   }
-  // const editStudents = (cedula) => {
-  //   for (let i = 0; i < students.length; i++) {
-  //     if(students[i].cedula == cedula)
-  //     {
-  //       // console.log(students[i].nombre)
-  //       document.getElementsByName('nombre').value = students[i].nombre
-  //     }
-  //   }
-  // }
 
-  const removeStudents = (cedula) => {
-    setStudents(students.filter(items => items.cedula !== cedula))
+  const eliminar = () => {
+    setStudent(student.filter(estudiante => estudiante.cedula !== estudianteSeleccionado.cedula));
+    setModalEliminar(false);
+  }
+
+  const abrirModalInsertar = () => {
+    setEstudianteSeleccionado(null);
+    setModalInsertar(true);
+  }
+
+  const insertar = () => {
+    var valorInsertar = estudianteSeleccionado;
+    var dataNueva = student;
+    dataNueva.push(valorInsertar);
+    setStudent(dataNueva);
+    setModalInsertar(false);
   }
 
   return (
-    <>
-      <Container className='mt-4'>
-
-        <FormGroup>
-          <label htmlFor="cedula">Cedula</label>
-          <input type="number" className="form-control" name='cedula' minLength={6} maxLength={8} value={cedula} onChange={(val) => setCedula(val.target.value)} />
-        </FormGroup>
-
-        <FormGroup>
-          <label htmlFor="nombre">Nombre</label>
-          <input type="text" className="form-control" name='nombre' id={nombre.toString()} value={nombre} onChange={(val) => setNombre(val.target.value)} />
-        </FormGroup>
-
-        <FormGroup>
-          <label htmlFor="apellido">Apellido</label>
-          <input type="text" className="form-control" name='apellido' value={apellido} onChange={(val) => setApellido(val.target.value)} />
-        </FormGroup>
-
-        <FormGroup>
-          <label htmlFor="birthday">Fecha de Nacimiento</label>
-          <input type="date" className="form-control" name='birthday' value={birthday} onChange={(val) => setBirthday(val.target.value)} />
-        </FormGroup>
-
-        <FormGroup>
-          <label htmlFor="genero">Genero</label>
-          <select
-            className='form-select'
-            value={genero}
-            onChange={(val) => setGenero(val.target.value)}>
-            <option value="">Seleccionar</option>
-            {
-              arrGenero.map((genero, index) => (
-                <option key={index} value={genero.value}> {genero.value} </option>
-              ))
-            }
-          </select>
-        </FormGroup>
-
-        <FormGroup>
-          <label htmlFor="seccion">Seccion</label>
-          <select
-            className='form-select'
-            value={seccion}
-            onChange={(val) => setSeccion(val.target.value)}>
-            <option value="">Seleccionar</option>
-            {
-              arrSeccion.map((seccion, index) => (
-                <option key={index} value={seccion.value}> {seccion.value} </option>
-              ))
-            }
-          </select>
-        </FormGroup>
-
-        <Button color='success' onClick={() => addStudents()}>Agregar nuevo estudiante</Button>
-
-        <h1 className="mt-3 mb-5 text-center">Lista de Estudiantes</h1>
-        <Table>
-          <thead className='text-center'>
-            <tr>
-              <th>Cedula</th>
-              <th>Nombre</th>
-              <th>Apellido</th>
-              <th>Fecha de Nacimiento</th>
-              <th>Genero</th>
-              <th>Seccion</th>
-              <th>Accion</th>
-            </tr>
-          </thead>
-          <tbody className='text-center'>
-            {
-              students.map((s, index) => (
-                <tr>
-                  <td key={index}>{s.cedula}</td>
-                  <td>{s.nombre}</td>
-                  <td>{s.apellido}</td>
-                  <td>{s.birthday}</td>
-                  <td>{s.genero}</td>
-                  <td>{s.seccion}</td>
-                  <td><Button color='warning' className='me-2' onClick={()=>studentSelected(elemento, 'Editar')}>Editar</Button><Button color='danger' onClick={() => removeStudents(s.cedula)}>Eliminar</Button></td>
-                </tr>
-              ))
-            }
-          </tbody>
-        </Table>
-      </Container>
+    <div className="App container">
+      <h2 className='text-center mt-5'>Lista de estudiantes del curso</h2>
+      <br />
+      <button className="btn btn-success w-100" onClick={() => abrirModalInsertar()}>Agregar nuevo estudiante</button>
+      <br /><br />
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Cedula</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Fecha de Nacimiento</th>
+            <th>Genero</th>
+            <th>Seccion</th>
+            <th>Accion</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            student.map((s, index) => (
+              <tr>
+                {/* <td key={index}>{s.cedula}</td> */}
+                <td>{s.nombre}</td>
+                <td>{s.apellido}</td>
+                {/* <td>{s.birthday}</td> */}
+                <td>{s.genero}</td>
+                <td>{s.seccion}</td>
+                <td>
+                  <button className="btn btn-primary" onClick={() => seleccionarEstudiante(s, 'Ver')}>Ver</button> {"   "}
+                  <button className="btn btn-warning" onClick={() => seleccionarEstudiante(s, 'Editar')}>Editar</button> {"   "}
+                  <button className="btn btn-danger" onClick={() => seleccionarEstudiante(s, 'Eliminar')}>Eliminar</button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
 
       <Modal isOpen={modalEditar}>
         <ModalHeader>
           <div>
-            <h3>Editar País</h3>
+            <h3>Editar Estudiante</h3>
           </div>
         </ModalHeader>
         <ModalBody>
@@ -219,10 +141,10 @@ function App() {
             <label>Cedula</label>
             <input
               className="form-control"
+              readOnly
               type="number"
               name="cedula"
-              value={studentSelected && studentSelected.cedula}
-              onChange={handleChange}
+              value={estudianteSeleccionado && estudianteSeleccionado.cedula}
             />
             <br />
 
@@ -231,7 +153,7 @@ function App() {
               className="form-control"
               type="text"
               name="nombre"
-              value={studentSelected && studentSelected.nombre}
+              value={estudianteSeleccionado && estudianteSeleccionado.nombre}
               onChange={handleChange}
             />
             <br />
@@ -241,9 +163,49 @@ function App() {
               className="form-control"
               type="text"
               name="apellido"
-              value={studentSelected && studentSelected.apellido}
+              value={estudianteSeleccionado && estudianteSeleccionado.apellido}
               onChange={handleChange}
             />
+            <br />
+
+            <label>Fecha de nacimiento</label>
+            <input
+              className="form-control"
+              type="date"
+              name="birthday"
+              value={estudianteSeleccionado && estudianteSeleccionado.birthday}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Genero</label>
+            <select
+              className='form-select'
+              name="genero"
+              value={estudianteSeleccionado && estudianteSeleccionado.genero}
+              onChange={handleChange}>
+              <option value="">Seleccionar</option>
+              {
+                arrGenero.map((genero, index) => (
+                  <option key={index} value={genero.value}> {genero.value} </option>
+                ))
+              }
+            </select>
+            <br />
+
+            <label>Seccion</label>
+            <select
+              className='form-select'
+              name="seccion"
+              value={estudianteSeleccionado && estudianteSeleccionado.seccion}
+              onChange={handleChange}>
+              <option value="">Seleccionar</option>
+              {
+                arrSeccion.map((seccion, index) => (
+                  <option key={index} value={seccion.value}> {seccion.value} </option>
+                ))
+              }
+            </select>
             <br />
           </div>
         </ModalBody>
@@ -259,7 +221,174 @@ function App() {
           </button>
         </ModalFooter>
       </Modal>
-    </>
+
+
+      <Modal isOpen={modalEliminar}>
+        <ModalBody>
+          Estás seguro que deseas eliminar al estudiante con la cedula {estudianteSeleccionado && estudianteSeleccionado.cedula} ?
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-danger" onClick={() => eliminar()}>
+            Sí
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => setModalEliminar(false)}
+          >
+            No
+          </button>
+        </ModalFooter>
+      </Modal>
+
+
+      <Modal isOpen={modalInsertar}>
+        <ModalHeader>
+          <div>
+            <h3>Agregar estudiante</h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Cedula</label>
+            <input
+              className="form-control"
+              type="number"
+              name="cedula"
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Nombre</label>
+            <input
+              className="form-control"
+              type="text"
+              name="nombre"
+              value={estudianteSeleccionado ? estudianteSeleccionado.nombre : ''}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Apellido</label>
+            <input
+              className="form-control"
+              type="text"
+              name="apellido"
+              value={estudianteSeleccionado ? estudianteSeleccionado.apellido : ''}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Fecha de nacimiento</label>
+            <input
+              className="form-control"
+              type="date"
+              name="birthday"
+              value={estudianteSeleccionado ? estudianteSeleccionado.birthday : ''}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Genero</label>
+            <select
+              className='form-select'
+              name="genero"
+              value={estudianteSeleccionado ? estudianteSeleccionado.genero : ''}
+              onChange={handleChange}>
+              <option value="">Seleccionar</option>
+              {
+                arrGenero.map((genero, index) => (
+                  <option key={index} value={genero.value}> {genero.value} </option>
+                ))
+              }
+            </select>
+            <br />
+
+            <label>Seccion</label>
+            <select
+              className='form-select'
+              name="seccion"
+              value={estudianteSeleccionado ? estudianteSeleccionado.seccion : ''}
+              onChange={handleChange}>
+              <option value="">Seleccionar</option>
+              {
+                arrSeccion.map((seccion, index) => (
+                  <option key={index} value={seccion.value}> {seccion.value} </option>
+                ))
+              }
+            </select>
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary"
+            onClick={() => insertar()}>
+            Insertar
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => setModalInsertar(false)}
+          >
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalVer}>
+        <ModalHeader>
+          <div>
+            <h3>Detalle de Estudiante</h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          {
+            student.map((s, index) => (
+              <div className="row text-center">
+                <div className="col-sm-6">
+                  <strong>Cedula</strong>
+                  <br />
+                  {s.cedula}
+                </div>
+                <div className="col-sm-6">
+                  <strong>Fecha de nacimiento</strong>
+                  <br />
+                  {s.birthday}
+                </div>
+                
+                <div className="col-sm-6 mt-3">
+                  <strong>Nombre</strong>
+                  <br />
+                  {s.nombre}
+                </div>
+                <div className="col-sm-6 mt-3">
+                  <strong>Apellido</strong>
+                  <br />
+                  {s.apellido}
+                </div>
+
+                <div className="col-sm-6 mt-3">
+                  <strong>Género</strong>
+                  <br />
+                  {s.genero}
+                </div>
+                <div className="col-sm-6 mt-3">
+                  <strong>Sección</strong>
+                  <br />
+                  {s.seccion}
+                </div>
+              </div>  
+            ))
+          }
+        </ModalBody>
+        <ModalFooter>
+          <button
+            className="btn btn-danger"
+            onClick={() => setModalVer(false)}
+          >
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
+    </div>
   );
 }
 
